39876baba5ce0bae0c5d790e17b60aa4617ba9ca
Issy Hanna <ih315@cam.ac.uk>
2021-03-05 17:19:02 +0000

    Give NPCs names

--- index.js
+++ index.js
@@ -131,7 +131,17 @@ class NPC {
     constructor(lenOfArr) {
         this.position = Math.floor(Math.random()*lenOfArr)+1;
         this.RPSChoice = Math.floor(Math.random()*3);
+        this.name = NPC.Generatename();
     }
+
+    static Generatename(){
+        let Firstname = ["Gold","High","Old","Wild","Lonely"]
+        let Lastname = ["Witch","Sage","Joker","Troll"]
+        let FNnum = Math.floor(Math.random() * 5);
+        let LNnum = Math.floor(Math.random() * 4);
+        return(Firstname[FNnum]+' ' +Lastname[LNnum]);
+  
+      }
 }
 
 class Dragon {
