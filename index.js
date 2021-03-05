efd57c25502d70098bacc094463f7e048a631f5a
aselperera <aselperera@hotmail.com>
2021-03-05 16:17:03 +0000

    First draft finished

--- index.js
+++ index.js
@@ -22,6 +22,8 @@ class Game {
             this[`npc${i}`] = new NPC(this.difficultyTranslator[userDifficulty].lenOfArr);
         }
 
+        // Initiate dragon
+        this.dragon = new Dragon(this.difficultyTranslator[userDifficulty].lenOfArr);
     };
 
     difficultyTranslator = {
@@ -48,6 +50,44 @@ class Game {
         return roll;
     };
 
+    RPSGen(computerMove){
+        //e.g.{rock:2,scissors:1, paper:0}
+        var playerMove = Math.floor(Math.random() * 3);
+        let result;
+        
+        if ((computerMove + 1) % 3 == playerMove){
+            result = +1;
+        }else if(computerMove == playerMove){
+            result = 0;
+        }else{
+            result = -1;
+        }
+        console.log(result);
+        return(result);
+    }
+
+    fightDragon(playerItems) {
+        let odds = 6 - playerItems;
+        let dragonMove = Math.floor((Math.random() * odds) + 1);
+
+        let playerMove = parseInt(prompter(`Please guess a number between 1 and ${odds}: >>>> `));
+    
+        if (dragonMove === playerMove){
+            console.log(`You guessed: ${playerMove}`);
+            console.log(`Dragon guessed: ${dragonMove}`);
+            console.log("You have won the game!");
+            this.endGame = true;
+        }else{
+            console.log(`You guessed: ${playerMove}`);
+            console.log(`Dragon guessed: ${dragonMove}`);
+            console.log("You lose n00b");
+            //this.endGame = true;
+            // Add option to let user play again
+            this.player.position = 0;
+            this.player.items = 0;
+        }
+    }
+
     playRound() {
         this.player.position += this.rolldice();
         console.log("New player position:", this.player.position);
@@ -55,12 +95,19 @@ class Game {
         for (let i = 0; i < this.difficultyTranslator[this.difficulty].numOfNpc; i++) {
             if (this.player.position === this[`npc${i}`].position) {
                 console.log("RPS");
+                this.player.items += this.RPSGen(this[`npc${i}`].RPSChoice);
+                this.player.items = Math.max(this.player.items, 0); // Stops player items going negative
+                console.log(this.player.items);
                 break;
             }
         }
 
-        game.endGame = true;
+        // Check if player is >= end
+        if (this.player.position >= this.difficultyTranslator[userDifficulty].lenOfArr-1) {
+            this.fightDragon(this.player.items);
+        }
     }
+    
 }
 
 class Map {
@@ -88,7 +135,9 @@ class NPC {
 }
 
 class Dragon {
-
+    constructor(lenOfArr) {
+        this.position = lenOfArr-1;
+    }
 }
 
 let game = new Game(userDifficulty);
