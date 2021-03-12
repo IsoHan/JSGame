9acaabfb910b99bad702fe70d7267c6a8ed984ec
aselperera <aselperera@hotmail.com>
2021-03-12 16:01:02 +0000

    Used inquirer to ask difficulty question

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 import prompt from "prompt-sync";
 import fs from "fs";
+import inquirer from "inquirer";
 import { Game } from "./classes/game.js"
 let prompter = prompt();
 
+// Welcome Title
 let title = JSON.parse(fs.readFileSync("./game_data/text.json", "utf-8"));
 console.log(title);
-
 console.log("Welcome to our game!");
-let userDifficulty = prompter("Please select your level of difficulty: easy, medium, hard. >>>> ").toLowerCase();
-console.log("Difficulty selected:", userDifficulty);
+
+// New Difficulty Selection - using Inquirer
+let userDifficulty = await inquirer.prompt({
+    type: "list",
+    message: "Please select your level of difficulty: easy, medium, hard.",
+    name: "response",
+    choices: ["easy", "medium", "hard"]
+})
+.then((response) => {
+    return response["response"]
+})
+
+// Difficulty Selection
+// let userDifficulty = prompter("Please select your level of difficulty: easy, medium, hard. >>>> ").toLowerCase();
+// console.log("Difficulty selected:", userDifficulty);
 
 let game = new Game(userDifficulty);
 console.log(game);
